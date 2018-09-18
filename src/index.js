6878162118bfdd1835c17d98ca38cf8a986eaa37
Pavel <SmetaninPavel@users.noreply.github.com>
2018-09-18 16:34:19 +0300

    [04] Quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const equationArray = equation.split(' ');
+
+  const a = +(equationArray[0]);
+  const b = +(equationArray[3] + equationArray[4]);
+  const c = +(equationArray[7] + equationArray[8]);
+
+  const D = (b * b) - (4 * a * c);
+
+  if (D < 0) {
+    return [];
+  } else if (D === 0) {
+    const x = Math.round((- b) / (2 * a));
+    return [x];
+  } else {
+    const x1 = Math.round((-b + Math.sqrt(D)) / (2 * a));
+    const x2 = Math.round((-b - Math.sqrt(D)) / (2 * a));
+
+    return [x1, x2].sort((a, b) => a - b);
+  }
 }
